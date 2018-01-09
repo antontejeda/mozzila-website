30069ef7b9230f0d95b2dd1d43cfaeffc13c96cb
Anton Tejeda <edt371@student@hks.harvard.edu>
2018-01-09 16:36:00 -0500

    Adding chart to mozilla website

---
+++ scripts/chart.js
@@ -0,0 +1,22 @@
+var margin = {top: 25, right: 50, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var parseDate = d3.time.format("%d-%b-%y").parse,
+    bisectDate = d3.bisector(function(d) { return d.date; }).left,
+    formatValue = d3.format(",.2f"),
+    formatCurrency = function(d) { return "$" + formatValue(d); };
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
